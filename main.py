b89ccc686d408bdaffa3ddab45496b13da86da20
Kravchenko Igor <slipko89@gmail.com>
2019-04-03 23:26:26 +0300

    1sr release

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import csv
 
 #Read file arp.csv
-with open('/home/slipko/PycharmProjects/arp-vm/arp.csv') as arp:
+with open('arp.csv') as arp:
     reader = csv.reader(arp,delimiter=';')
     arp_list = list(reader)
 
@@ -10,7 +10,7 @@ a = len(arp_list)
 print ("Lenght of arp.csv= " ,a)
 
 #Read file vm_mac.csv
-with open('/home/slipko/PycharmProjects/arp-vm/vm_mac.csv') as vm:
+with open('vm_mac.csv') as vm:
     reader = csv.reader(vm,delimiter=';')
     vm_list = list(reader)
 
@@ -30,13 +30,24 @@ while i < b:
 i = 0
 hop = 0
 
+final_list=list()
+
 while i < a:
     n = 0
     while n < b:
         if arp_list[i][1] == vm_list[n][1]:
-            #print (arp_list[i][0],arp_list[i][1],vm_list[n][0],vm_list[n][1])
-            hop = hop + 1
+             #print (arp_list[i][0],arp_list[i][1],vm_list[n][0],vm_list[n][1])
+             final_list.append([arp_list[i][0],vm_list[n][0]])
+             hop = hop + 1
         n = n + 1
     i = i + 1
 
-#print (hop)
+# Sort final_list
+final_list.sort()
+print(final_list)
+
+# Write CSV file
+with open('final.csv', 'w') as final:
+    writer = csv.writer(final, delimiter=',')
+    writer.writerow(["IP Address","Name"])  # write header
+    writer.writerows(final_list)
