96720df2fd8f55daccffdd67390b20d36dc06458
Kravchenko Igor <slipko89@gmail.com>
2019-04-03 22:39:06 +0300

    1sr release

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
 import csv
 
-print ("Read file arp.csv")
+#Read file arp.csv
 with open('/home/slipko/PycharmProjects/arp-vm/arp.csv') as arp:
     reader = csv.reader(arp,delimiter=';')
     arp_list = list(reader)
 
-print ("Read file vm_mac.csv")
+a = len(arp_list)
+print ("Lenght of arp.csv= " ,a)
+
+#Read file vm_mac.csv
 with open('/home/slipko/PycharmProjects/arp-vm/vm_mac.csv') as vm:
     reader = csv.reader(vm,delimiter=';')
     vm_list = list(reader)
-
-a = len(arp_list)
-print ("Lenght of arp.csv= " ,a)
 
 b = len(vm_list)
 print ("Lenght of vm.csv= " ,b)
@@ -34,11 +34,9 @@ while i < a:
     n = 0
     while n < b:
         if arp_list[i][1] == vm_list[n][1]:
-            print (arp_list[i][0],arp_list[i][1],vm_list[n][0],vm_list[n][1])
+            #print (arp_list[i][0],arp_list[i][1],vm_list[n][0],vm_list[n][1])
             hop = hop + 1
         n = n + 1
     i = i + 1
-#    i = i + 1
-#
-print (hop)
 
+#print (hop)
